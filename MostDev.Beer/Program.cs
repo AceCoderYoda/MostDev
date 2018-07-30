56a9a555e9667cea60e1f7828babf21645014c51
AceCoderYoda <workingmanforums@outlook.com>
2018-07-30 10:33:28 +0200

    added beer options

--- MostDev.Beer/Program.cs
+++ MostDev.Beer/Program.cs
@@ -9,6 +9,9 @@
 #region ...   [Usings]   ...
 
 using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
 
 #endregion
 
@@ -20,6 +23,9 @@ namespace MostDev.Beer
     /// </summary>
     internal class Program
     {
+        private  const int interval= 1500;
+       
+
         private static void Main(string[] args)
         {
             Console.WriteLine("Welcome to beer-time!");
@@ -36,13 +42,19 @@ namespace MostDev.Beer
             {
                 Console.WriteLine($"{i} bottles of beer on the wall, {i} bottles of beer.");
                 Console.WriteLine($"Take one down and pass it around, {i--} bottles of beer on the wall.");
+                Console.WriteLine(Environment.NewLine);
+                Thread.Sleep(interval);
             }
 
             Console.WriteLine($"1 bottle of beer on the wall, 1 bottles of beer.");
             Console.WriteLine($"Take 1 down and pass it around, no more bottles of beer on the wall.");
+            Console.WriteLine(Environment.NewLine);
+            Thread.Sleep(interval);
 
             Console.WriteLine($"No more bottles of beer on the wall, no more bottles of beer. ");
             Console.WriteLine($"Go to the store and buy some more, 99 bottles of beer on the wall.");
+            Console.WriteLine(Environment.NewLine);
+            Thread.Sleep(interval);
 
             Console.WriteLine("We are done!");
             Console.WriteLine("Press the <anykey>");
@@ -58,21 +70,61 @@ namespace MostDev.Beer
                 {
                     Console.WriteLine($"{i} bottles of beer on the wall, {i} bottles of beer.");
                     Console.WriteLine($"Take one down and pass it around, {i--} bottles of beer on the wall.");
+                    Console.WriteLine(Environment.NewLine);
+                    Thread.Sleep(interval);
                 }
                 else if (i == 1)
                 {
                     Console.WriteLine($"{i} bottle of beer on the wall, {i} bottles of beer.");
                     Console.WriteLine($"Take {i--} down and pass it around, no more bottles of beer on the wall.");
+                    Console.WriteLine(Environment.NewLine);
+                    Thread.Sleep(interval);
                 }
                 else
                 {
                     Console.WriteLine($"No more bottles of beer on the wall, no more bottles of beer. ");
                     Console.WriteLine($"Go to the store and buy some more, 99 bottles of beer on the wall.");
+                    Console.WriteLine(Environment.NewLine);
+                    Thread.Sleep(interval);
                 }
 
             Console.WriteLine("We are done!");
             Console.WriteLine("Press the <anykey>");
             Console.ReadKey();
+            BeerTime2();
+        }
+
+        private static void BeerTime3()
+        {
+            StringBuilder beerLyric = new StringBuilder();
+           var beers =
+                (from n in Enumerable.Range(0, 100)
+                    select new
+                    {
+                        Say = n == 0 ? "No more bottles" :
+                            (n == 1 ? "1 bottle" : n.ToString() + " bottles"),
+                        Next = n == 1 ? "no more bottles" :
+                            (n == 0 ? "99 bottles" :
+                                (n == 2 ? "1 bottle" : n.ToString() + " bottles")),
+                        Action = n == 0 ? "Go to the store and buy some more" :
+                            "Take one down and pass it around"
+                    }).Reverse();
+
+            foreach (var beer in beers)
+            {
+                beerLyric.AppendFormat("{0} of beer on the wall, {1} of beer.{2}",
+                    beer.Say, beer.Say.ToLower(), Environment.NewLine);
+                beerLyric.AppendFormat("{0}, {1} of beer on the wall.{2}",
+                    beer.Action, beer.Next, Environment.NewLine);
+                beerLyric.AppendLine();
+            }
+
+            Console.WriteLine(beerLyric.ToString());
+
+            Console.WriteLine("We are done!");
+            Console.WriteLine("Press the <anykey>");
+            Console.ReadKey();
+            BeerTime3();
         }
     }
 }
